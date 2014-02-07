7abdf8aeed6008a9f5d5bf72a6300aa269a81dc9
vlad <vlad@v-lad.org>
2014-02-07 17:29:39 -0500

    Removed long help and autocomplete

--- include/configs/mx6dl_pplans.h
+++ include/configs/mx6dl_pplans.h
@@ -119,9 +119,7 @@
 /*
  * Miscellaneous configurable options
  */
-#define CONFIG_SYS_LONGHELP		/* undef to save memory */
 #define CONFIG_SYS_PROMPT		"MX6SDL SABRESD-MFG U-Boot > "
-#define CONFIG_AUTO_COMPLETE
 #define CONFIG_SYS_CBSIZE		256	/* Console I/O Buffer Size */
 /* Print Buffer Size */
 #define CONFIG_SYS_PBSIZE (CONFIG_SYS_CBSIZE + sizeof(CONFIG_SYS_PROMPT) + 16)
