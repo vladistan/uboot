92e31e16703f8208613a3a919c8e91cdcf91ad61
vlad <vlad@v-lad.org>
2014-02-11 00:18:17 -0500

    Now PMIC is correctly programmed for our board..

--- include/configs/mx6dl_pplans_iram.h
+++ include/configs/mx6dl_pplans_iram.h
@@ -151,7 +151,7 @@
 #ifdef CONFIG_CMD_I2C
 	#define CONFIG_HARD_I2C         1
 	#define CONFIG_I2C_MXC          1
-	#define CONFIG_SYS_I2C_PORT             I2C3_BASE_ADDR
+	#define CONFIG_SYS_I2C_PORT             I2C1_BASE_ADDR
 	#define CONFIG_SYS_I2C_SPEED            100000
 	#define CONFIG_SYS_I2C_SLAVE            0x1f
 #endif
