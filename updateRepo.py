ed5977656a402208587df0887613e899b08b73cb
Vlad <vlad@v-lad.org>
2015-12-23 22:43:35 -0500

    Set build url on the uploaded artifacts

--- updateRepo.py
+++ updateRepo.py
@@ -1,12 +1,15 @@
 import boto
 import sys
+import os
 
 PKG = sys.argv[1]
 BUILD = sys.argv[2]
+URL = os.environ['BUILD_URL']
 
 
 print "PKG %s" % PKG
 print "Build version %s" % BUILD
+print "Traceback URL %s" % URL
 
 from boto.s3.key import Key
 
@@ -15,7 +18,7 @@ b = c.get_bucket('pplansbuildtools')
 
 k = Key(b)
 k.key = "%s/defaultStage/defaultJob/1.0.%s/" % (PKG, BUILD)
-k.set_metadata('user','vlad')
-k.set_metadata('traceback_url','http://www.google.com')
-k.set_metadata('completed','yes')
+k.set_metadata('user', 'vlad')
+k.set_metadata('traceback_url', URL)
+k.set_metadata('completed', 'yes')
 k.set_contents_from_string('')
